58ae5921147e93ba0d41dd6cb7ac4d6dfef081e3
Ammar Yasser <yourgitemail@example.com>
2023-03-08 21:50:04 +0200

    instantiate connection once

--- worker/worker.py
+++ worker/worker.py
@@ -1,4 +1,5 @@
 import paho.mqtt.client as mqtt
+from db.client import connect
 import json
 
 
@@ -14,12 +15,17 @@ def on_message(client, userdata, msg):
     commit_message(message)
 
 
+def main(): 
+    session = connect()
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+    client.connect("localhost", 1883, 60)
 
-client.connect("localhost", 1883, 60)
+    client.loop_forever()
 
-client.loop_forever()
+
+if __name__=="__main__":
+    main()
 
