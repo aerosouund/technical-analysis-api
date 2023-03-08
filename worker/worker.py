beade1f3193f6f9e18098ce7bae67cfa59e0acf0
Ammar Yasser <yourgitemail@example.com>
2023-03-08 21:37:59 +0200

    clean worker

--- worker/worker.py
+++ worker/worker.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
-import psycopg2
+import json
+
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
@@ -10,23 +11,9 @@ def on_message(client, userdata, msg):
 
     # parse the incoming JSON message
     message = json.loads(msg.payload)
-    stock_id = message['stock_id']
-    name = message['name']
-    price = message['price']
-    availability = message['availability']
-    timestamp = message['timestamp']
+    commit_message(message)
 
-    # connect to the PostgreSQL database
-    conn = psycopg2.connect(host="localhost", database="your_database_name", user="your_username", password="your_password")
-    cur = conn.cursor()
 
-    # insert the message into the table
-    cur.execute("INSERT INTO stock_data (stock_id, name, price, availability, timestamp) VALUES (%s, %s, %s, %s, %s)", (stock_id, name, price, availability, timestamp))
-    conn.commit()
-
-    # close the database connection
-    cur.close()
-    conn.close()
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -35,3 +22,4 @@ client.on_message = on_message
 client.connect("localhost", 1883, 60)
 
 client.loop_forever()
+
