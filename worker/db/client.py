779132fb7d20f56e763eff84156efb8d38ef0fd3
Ammar Yasser <yourgitemail@example.com>
2023-03-08 21:37:36 +0200

    db client

---
+++ worker/db/client.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+def connect():
+    engine = create_engine('postgresql://your_username:your_password@localhost/your_database_name')
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+
+def commit_message(message):
+    # parse the incoming JSON message
+    stock_id = message['stock_id']
+    name = message['name']
+    price = message['price']
+    availability = message['availability']
+    timestamp = message['timestamp']
+
+
+    # create a StockData object and insert it into the database
+    stock_data = StockData(stock_id=stock_id, name=name, price=price, availability=availability, timestamp=timestamp)
+    session.add(stock_data)
+    session.commit()
+
+    # close the session
+    session.close()
