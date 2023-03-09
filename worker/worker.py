54b35a695ee69e03c2b229f4015bd392ad803d72
Ammar Yasser <yourgitemail@example.com>
2023-03-09 21:52:49 +0200

    remove comments for now

--- worker/worker.py
+++ worker/worker.py
@@ -9,7 +9,6 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
-    # parse the incoming JSON message
     message = json.loads(msg.payload)
     commit_message(message)
 
