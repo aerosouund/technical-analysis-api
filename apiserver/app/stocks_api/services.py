581e420d61e9a361e0712da055610b5807027dd0
Ammar Yasser <yourgitemail@example.com>
2023-03-11 18:29:08 +0200

    maybe no influx db ?

--- apiserver/app/stocks_api/services.py
+++ apiserver/app/stocks_api/services.py
@@ -5,18 +5,21 @@ from typing import Dict, List
 from app import db
 from app.stocks_api.models import Stock
 from app.stocks_api.schemas import StockSchema
+from sqlalchemy.sql.expression import func
+
 
 logging.basicConfig(level=logging.WARNING)
-logger = logging.getLogger("udaconnect-api")
+logger = logging.getLogger("stocks-api")
 
 
 class StockService:
+    @staticmethod
+    def retrieve(stock_id: string) -> Stock:
+        stock = session.query(func.max(Stock.creation_time)).filter(
+        Stock.stock_id == stock_id
+    )
+        return stock
 
     @staticmethod
-    def retrieve(person_id: int) -> Person:
-        person = db.session.query(Person).get(person_id)
-        return person
-
-    @staticmethod
-    def retrieve_all() -> List[Person]:
-        return db.session.query(Person).all()
+    def retrieve_all() -> List[Stock]:
+        return db.session.query(Stock).all()
