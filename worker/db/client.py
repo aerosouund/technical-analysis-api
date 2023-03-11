b68faa78a923f4d89cda050a1f22e3fa8ee4a40f
Ammar Yasser <yourgitemail@example.com>
2023-03-11 20:04:48 +0200

    data modelling succeded

--- worker/db/client.py
+++ worker/db/client.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from db.models import Stock, get_stock_model
+from db.models import get_stock_model
+from sqlalchemy import Column, Integer, String, DateTime, MetaData, Table
+from sqlalchemy.inspection import inspect
 import logging
 
 def connect():
@@ -8,6 +10,23 @@ def connect():
     Session = sessionmaker(bind=engine)
     session = Session()
     return session
+
+
+def create_stock_table(name):
+    engine = create_engine('mysql://admin:thndr@127.0.0.1:3306/stocks')
+
+    if not inspect(engine).has_table('stock_{}'.format(name).replace(' ', '_')):  # If table don't exist, Create.
+        meta = MetaData()
+        stock_table = Table(
+        'stock_{}'.format(name).replace(' ', '_'), meta, 
+        Column('stock_id', String(50)), 
+        Column('name', String(10)), 
+        Column('price', Integer),
+        Column('availability', Integer), 
+        Column('timestamp', DateTime, index=True)
+        )
+
+        meta.create_all(engine, checkfirst=True)
 
 
 def commit_message(message):
@@ -21,7 +40,8 @@ def commit_message(message):
 
 
     # create a StockData object and insert it into the database
-    stock_data = get_stock_model(name)(stock_id=stock_id, name=name, price=price, availability=availability, timestamp=timestamp)
+    create_stock_table(name)
+    stock_data = get_stock_model(name.replace(' ', '_'))(stock_id=stock_id, name=name, price=price, availability=availability, timestamp=timestamp)
     session.add(stock_data)
     session.commit()
 
--- worker/db/models.py
+++ worker/db/models.py
@@ -6,18 +6,19 @@ Base = declarative_base()
 class Stock(Base):
     __abstract__ = True
 
-    id = Column(Integer, primary_key=True)
     stock_id = Column(String)
     name = Column(String)
     price = Column(Integer)
     availability = Column(Integer)
-    timestamp = Column(DateTime)
+    timestamp = Column(DateTime, primary_key=True)
 
 
 def get_stock_model(name):
     tablename = 'stock_%s' % name  # dynamic table name
     class_name = 'Stock%s' % name  # dynamic class name
     Model = type(class_name, (Stock,), {
-        '__tablename__': tablename
+        '__tablename__': tablename,
+        '__table_args__': {'extend_existing': True}
+
     })
     return Model
