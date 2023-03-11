e7f4eeb9c019a086215b0b82892f221e23c0f1f4
Ammar Yasser <yourgitemail@example.com>
2023-03-11 20:52:11 +0200

    comment

--- apiserver/app/stocks_api/services.py
+++ apiserver/app/stocks_api/services.py
@@ -14,10 +14,10 @@ logger = logging.getLogger("stocks-api")
 
 class StockService:
     @staticmethod
-    def retrieve(stock_id: string):
+    def retrieve(stock_id):
         stock = session.query(func.max(get_stock_model(stock_id).creation_time)).filter
         return stock
 
-    @staticmethod
-    def retrieve_all() -> List[Stock]:
-        return db.session.query(Stock).all()
+    # @staticmethod
+    # def retrieve_all() -> List[Stock]:
+    #     return db.session.query(Stock).all()
