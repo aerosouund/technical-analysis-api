ff9262e60330ab735aea240e597a816d625c8ea9
Ammar Yasser <yourgitemail@example.com>
2023-03-14 21:43:15 +0200

    remove api root

--- apiserver/app/stocks_api/__init__.py
+++ apiserver/app/stocks_api/__init__.py
@@ -2,7 +2,7 @@ from app.stocks_api.models import Stock
 from app.stocks_api.schemas import StockSchema
 
 
-def register_routes(api, app, root="api"):
+def register_routes(api, app, root=""):
     from app.stocks_api.controllers import api as stocks_api
 
     api.add_namespace(stocks_api, path=f"/{root}")
