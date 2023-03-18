b6ce0926c52117ebd68f79486043ab7fbda13891
Ammar Yasser <yourgitemail@example.com>
2023-03-18 20:57:16 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,4 @@ helm repo add my-repo https://charts.bitnami.com/bitnami
 helm install mysql bitnami/mysql --values values/mysql.yaml
 helm install redis bitnami/redis --values values/redis.yaml
 helm install prometheus prometheus-community/kube-prometheus-stack
+helm install thndr-app helm/thndr-technical-analysis/ --values helm/thndr-technical-analysis/values.yaml
