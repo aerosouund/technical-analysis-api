91406e3a292ec6f5e9ff335efeb025c57a6bb52d
Ammar Yasser <yourgitemail@example.com>
2023-03-18 18:47:22 +0200

    add logger

--- apiserver/app/__init__.py
+++ apiserver/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, g
 from flask_restx import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_redis import Redis
+import logging
 import json
 
 db = SQLAlchemy()
@@ -15,7 +16,7 @@ def create_app(env=None):
     app = Flask(__name__)
     app.config.from_object(config_by_name[env or "test"])
     api = Api(app, title="Stocks API", version="0.1.0")
-
+    app.logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
     register_routes(api, app)
     db.init_app(app)
